e6fe1146db8c2a6f2d9ea06d1993690691890910
skumar1 <skumar1@vmware.com>
2019-06-12 23:17:43 +0530

    Moved the data processing class to different project and refer in the collections test main program

---
+++ LearnCS/DataProcessingClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCS
+{
+    public class DataProcessingClass
+    {
+        public void WordCount(String Sentence)
+        {
+            List<String> wordList = Sentence.Split(null).ToList();
+            Dictionary<String, int> mydict = new Dictionary<String, int>();
+
+            foreach (String str in wordList)
+            {
+                Console.WriteLine(str);
+                if (mydict.ContainsKey(str))
+                {
+                    mydict[str] += 1;
+                }
+                else
+                {
+                    mydict.Add(str, 1);
+                }
+
+            }
+
+            foreach (String str in mydict.Keys)
+            {
+                Console.WriteLine("\nString " + str + " occured " + mydict[str] + "times\n");
+            }
+
+
+        }
+    }
+}
--- LearnCollections/Program.cs
+++ LearnCollections/Program.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Configuration;
 using System.Linq;
 using System.Collections;
+using LearnCS;
 
 namespace LearnCollections
 {
@@ -13,51 +14,12 @@ namespace LearnCollections
         public static void Main()
         {
             Console.WriteLine("Test Started...");
-            WordCount("hello how are you");
-            WordCount("hi hi hi hi");
+            DataProcessingClass dpc = new DataProcessingClass();
+            dpc.WordCount("hello hello how are you");
+            dpc.WordCount("hi hi hi hi");
             int[] numlist = new int[] { 4, 3, 2, 3, 4, 5, 4, 3, 2, 1, -1, 1, 2, 3, 4, 5 };
-            SearchArray(numlist, 5);
             Thread.Sleep(3000);
 
-        }
-
-        private static void SearchArray(int[] numlist, int v)
-        {
-            
-        }
-
-        public void TestList()
-        {
-
-        }
-
-        public static void WordCount(String Sentence)
-        {
-            List<String> wordList = Sentence.Split(null).ToList();
-            Dictionary<String, int> mydict = new Dictionary<String, int>();
-
-            foreach (String str in wordList)
-            {
-                Console.WriteLine(str);
-                if (mydict.ContainsKey(str))
-                {
-                    mydict[str] += 1;
-                }
-                else
-                {
-                    mydict.Add(str, 1);
-                }
-                
-            }
-
-            IDictionaryEnumerator dictiterator = mydict.GetEnumerator();
-
-            foreach(String str in mydict.Keys)
-            {
-                Console.WriteLine("\nString " + str + " occured " + mydict[str] + "times\n");
-            }
-            
-
-        }
+        }      
     }
 }
