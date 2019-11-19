4a87971aa4db35bda9b5ef4dcf21e330f7b69e02
skumar1 <skumar1@vmware.com>
2019-11-19 12:09:11 +0530

    Added main program and changes to test class learn

--- LearnCS/Program.cs
+++ LearnCS/Program.cs
@@ -7,17 +7,112 @@ using System.Threading.Tasks;
 
 namespace com.LearnCS
 {
+    public class Person
+    {
+        public string Name = "Default Name";
+        public DateTime BirthDate { get; set; }
+        public double Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - BirthDate;
+                var years = timeSpan.TotalDays / 365;
+                return years;
+
+            }
+        }
+
+
+        public void Introduce(string toperson)
+        {
+            Console.WriteLine("Hi {0} I am {1}", toperson, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var intperson = new Person
+            {
+                Name = str
+            };
+            return intperson;
+        }
+    }
+
+    public class Office
+    {
+        public int classNum;
+        public string className;
+        public readonly List<Person> People = new List<Person>();
+    }
     class Program
     {
 
         static void Main(string[] args)
         {
+
+            //Console.WriteLine("Test started" + args[0]);
+            System.Threading.Thread.Sleep(2000);
+
+            TestParamsANDObj();
+
+            System.Threading.Thread.Sleep(5000);
+
+            TestConstructorAndMethods();
+
+            TestStaticObjectreturn();
+
+            TestReadOnly();
+
+            System.Threading.Thread.Sleep(5000);
+
+
+        }
+
+        private static void TestReadOnly()
+        {
+            Office NewOffice = new Office();
+            NewOffice.People.Add(new Person());
+            NewOffice.People.Add(new Person());
+            NewOffice.People.Add(Person.Parse("emp"));
+            try
+            {
+                Console.WriteLine("\nTotal persons in office = " + NewOffice.People.Count + " and last person is " + NewOffice.People[NewOffice.People.Count - 1].Name);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Array out of index error");
+            }
+           
+        }
+
+        public static void TestParamsANDObj()
+        {
+            TestClassLearn newTCL = new TestClassLearn
+            {
+                Name = "Object Initializer"
+            };
+            Console.WriteLine("Name is " + newTCL.GetMeName());
+            try
+            {
+                newTCL.UseParams(1, 5, 9, "test", "args");
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Exception occured");
+            }
+        }
+
+        public static void TestConstructorAndMethods()
+        {
             int retInt;
             String retString;
             int conint;
-            Console.WriteLine("Test started" + args[0]);
-            System.Threading.Thread.Sleep(2000);
-            TestClassLearn tcl = new TestClassLearn();
+
+            TestClassLearn tcl = new TestClassLearn("Start Test");
+            Console.WriteLine("Second object name is {0}", tcl.Name);
+
             retInt = tcl.GetMeNumber(20);
             retString = tcl.GetMeString("hello");
             conint = int.Parse("100");
@@ -31,7 +126,14 @@ namespace com.LearnCS
             }
             Console.WriteLine(retString);
             Console.WriteLine("The string converted to int is " + conint);
-            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("\n nEW COURSE \n");
         }
+
+        public static void TestStaticObjectreturn()
+        {
+            var personvariable = Person.Parse("JOHN");
+            personvariable.Introduce("Mosh");
+        }
+
     }
 }
--- LearnCS/TestClassLearn.cs
+++ LearnCS/TestClassLearn.cs
@@ -8,17 +8,38 @@ namespace com.LearnCS
 {
     public class TestClassLearn
     {
+        public string Name = "Default";
         public TestClassLearn()
         {
-            Console.WriteLine("My first constructor");
+            Console.WriteLine("My constructor");
         }
+        public TestClassLearn(string argstr)
+            : this()
+        {
+            this.Name = argstr;
+            Console.WriteLine("Another constructor...\n");
+        }
+
         public int GetMeNumber(int testint)
         {
             return testint * 10;
         }
-        public String GetMeString(String teststr)
+        public void UseParams(params object[] listobjects)
+        {
+            if (listobjects == null)
+                throw new ArgumentNullException("list of objects");
+            foreach(object indobj in listobjects)
+            {
+                Console.WriteLine("Object is " + indobj);
+            }
+        }
+        public string GetMeString(String teststr)
         {
             return teststr + " added buddy";
+        }
+        public string GetMeName()
+        {
+            return Name;
         }
     }
 }
