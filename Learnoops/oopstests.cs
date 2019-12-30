92be6d470be7a2fae13dfe2f1b0c9a75aceed24d
skumar1 <skumar1@vmware.com>
2019-12-30 10:23:19 +0530

    Project for OOPS learning

---
+++ Learnoops/oopstests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Learnoops
+{
+    public class oopstests
+    {
+        [Fact]
+        public void testInheritance()
+        {
+            ChildClass cls = new ChildClass("New person");
+            Xunit.Assert.True(cls.gotName);
+            cls.setName(null);
+            Xunit.Assert.False(cls.gotName);
+        }
+        [Fact]
+        public void testComposition()
+        {
+            ComposeClass ccls = new ComposeClass(-300);
+            Xunit.Assert.True(ccls.outNumber > 0);
+        }
+    }
+
+}
